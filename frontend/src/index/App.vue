6a60a73e201c59c6989fc1d5fe0a9b9ec1b278ee
vignesh <52572934+vigneshpa@users.noreply.github.com>
2020-09-12 11:10:44 +0530

    Added offline fonts and icons

--- frontend/src/index/App.vue
+++ frontend/src/index/App.vue
@@ -28,6 +28,8 @@
     <v-app-bar
       app
       clipped-left
+      color = "blue"
+      dark
     >
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Chit Management system</v-toolbar-title>
@@ -38,32 +40,11 @@
         class="fill-height"
         fluid
       >
-        <v-row
-          align="center"
-          justify="center"
-        >
-          <v-col class="shrink">
-            <v-tooltip right>
-              <template v-slot:activator="{ on }">
-                <v-btn
-                  :href="source"
-                  icon
-                  large
-                  target="_blank"
-                  v-on="on"
-                >
-                  <v-icon large>mdi-code-tags</v-icon>
-                </v-btn>
-              </template>
-              <span>Source</span>
-            </v-tooltip>
-          </v-col>
-        </v-row>
       </v-container>
     </v-main>
 
-    <v-footer app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
+    <v-footer app dense>
+      <span>vigneshpa &copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
   </v-app>
 </template>
