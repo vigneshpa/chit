4c7e74c51962e763d64d675630366fbabc09aa3d
vignesh <vignesh-p@outlook.com>
2021-02-03 15:52:39 +0530

    adding manifest entries for placeholders

--- chit-renderer/vue.config.js
+++ chit-renderer/vue.config.js
@@ -12,8 +12,28 @@ module.exports = {
     },
   },
 
-  pwa:{
-    name:"Chit App"
+  pwa: {
+    name: "Chit App",
+    workboxOptions: {
+      additionalManifestEntries: [
+        {
+          revision: Math.floor(Math.random()*(10**10))+"",
+          url: "resources/browserSupport.js",
+        },
+        {
+          revision: Math.floor(Math.random()*(10**10))+"",
+          url: "resources/common.css",
+        },
+        {
+          revision: Math.floor(Math.random()*(10**10))+"",
+          url: "resources/sql-wasm.wasm",
+        },
+        {
+          revision: Math.floor(Math.random()*(10**10))+"",
+          url: "resources/worker.js",
+        },
+      ],
+    },
   },
 
   transpileDependencies: ["vuetify"],
