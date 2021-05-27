6ba5daf1de893b71a9acad4271b2f5610cf5e073
Vignesh Paranthaman <vignesh-p@outlook.com>
2021-05-27 21:31:50 +0530

    functional graphql

--- server/src/graphql/resolver.ts
+++ server/src/graphql/resolver.ts
@@ -8,8 +8,15 @@ export default class Resolver {
         this.name = userName;
         //Using default public schema untill creating user managemet
         const url = process.env.DATABASE_URL /* + "?schema=" + this.name */;
-        console.log(url);
         this.client = new PrismaClient({ datasources: { db: { url } } });
         this.root.user = () => this.client.user.findMany();
+        this.root.chit = () => this.client.chit.findMany();
+        this.root.group = () => this.client.group.findMany();
+
+        this.root.createUser = async ({ input }: any) => {
+            if (await this.client.user.findFirst({ where: { phone: input.phone } })) throw new Error("User with phone already exists");
+            const result = await this.client.user.create({ data: input }).finally();
+            return result;
+        }
     }
 }
