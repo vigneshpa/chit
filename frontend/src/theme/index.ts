f6f0143b2edf3defa84910d95b35621e31f03e86
Vignesh Paranthaman <vignesh-p@outlook.com>
2021-05-30 18:12:56 +0530

    moving options inside store

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -3,20 +3,20 @@ const optionsDefault = {};
 const storeDefault = {
     mobile:true as boolean,
     drawer:false as boolean,
+    options:optionsDefault
 };
 declare global {
     type TOptions = typeof optionsDefault;
     type TStore = typeof storeDefault;
 }
 export default class TTheme {
-    options: TOptions;
     store : TStore;
     constructor(options: TOptions = optionsDefault) {
-        this.options = ref(options).value;
         this.store = ref(storeDefault).value;
+        this.store.options = ref(options).value;
     }
     install(app: App, options: TOptions) {
-        this.options = options;
+        this.store.options = options;
         app.config.globalProperties.$TTheme = this;
         app.component("t-container", defineAsyncComponent(() => import("./TContainer.vue")));
         app.component("t-app", defineAsyncComponent(() => import("./TApp.vue")));
