029fc8b5d7a8677e0ad195e7813c944c1662586f
vignesh <vignesh-p@outlook.com>
2021-07-21 22:43:05 +0530

    improving find user

--- core/src/Actions/findUsers.ts
+++ core/src/Actions/findUsers.ts
@@ -1,3 +1,4 @@
+import type { User } from '../Entites';
 import type Repos from '../Entites';
 
 export default function makeFindUsers(repos: Repos) {
@@ -7,14 +8,16 @@ export default function makeFindUsers(repos: Repos) {
    * @param phone Phone number of the user
    * @param address Address of the user
    */
-  return async function findUsers({ name, phone, address }: { name?: string; phone?: string; address?: string }) {
-    // Validating
-    if (name && name.length < 3) throw new Error('Name must be longer than 2 characters');
-    if (phone && phone.length < 10) throw new Error('Phone number must be longer than 10 characters');
-    if (address && address.length < 5) throw new Error('Address must be longer than 5 characters');
+  return async function findUsers(params?: { partial: Partial<User> }) {
+    if (!(params && params.partial)) return Object.freeze(await repos.User.find());
+
+        // Validating
+        if (params.partial.name && params.partial.name.length < 3) throw new Error('Name must be longer than 2 characters');
+        if (params.partial.phone && params.partial.phone.length < 10) throw new Error('Phone number must be longer than 10 characters');
+        if (params.partial.address && params.partial.address.length < 5) throw new Error('Address must be longer than 5 characters');
 
     // Searching for the userrs
-    const users = await repos.User.find({ name, phone, address });
+    const users = await repos.User.find({ where: params.partial });
 
     return Object.freeze(users);
   };
