779f23ec9e1ab9cb37ab912dcf2b719626b6f907
vignesh <vignesh-p@outlook.com>
2021-07-23 22:37:55 +0530

    activating parent components

--- frontend/src/router/Router.svelte
+++ frontend/src/router/Router.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { getContext, setContext, hasContext } from 'svelte';
-  import navaid from 'navaid';
+  import navaid, { RouteHandler } from 'navaid';
   import type { Router } from 'navaid';
   import { writable } from 'svelte/store';
   import type { SvelteComponent, SvelteRouterContext, SvelteRouterMiddleware, SvelteRouterRoutes } from '.';
@@ -30,27 +30,33 @@
     setContext('svelte-navaid', ctx);
 
     // Function to generate context and handle routes
-    function handleRoutes(routes: SvelteRouterRoutes, router: Router, prepend: string = '') {
+    function handleRoutes(routes: SvelteRouterRoutes, router: Router, prepend: string = '', preRun: () => Promise<any> = async () => {}) {
       let component = writable<SvelteComponent | null>();
       let childCtx: SvelteRouterContext | undefined;
       for (const path in routes)
         if (Object.prototype.hasOwnProperty.call(routes, path)) {
-          const pageStr = prepend + '/' + path ;
+          const pageStr = prepend + '/' + path;
           console.log('Registering route', pageStr);
+
+          // Activate function
+          const activate: RouteHandler = async params => {
+            await preRun(); // Acrivating all parent components
+            component.set(await routes[path].component()); // Activating current component
+            childCtx?.component.set(null); // Closing child routes if exists
+          };
 
           // Registering route
           router.on(pageStr, async params => {
             window['svelte-router'].isLoading.set(true);
             window['svelte-router'].params.set(params);
-            component.set(await routes[path].component());  // Setting the page component
-            childCtx?.component.set(null);                  // Closing child routes if exists
+            await activate();
             window['svelte-router'].pageStr.set(pageStr);
             window['svelte-router'].isLoading.set(false);
           });
 
           // Registering subroutes
           if (routes[path].routes) {
-            childCtx = handleRoutes(<SvelteRouterRoutes>routes[path].routes, router, pageStr);
+            childCtx = handleRoutes(<SvelteRouterRoutes>routes[path].routes, router, pageStr, () => activate());
           }
         }
       return { component, childCtx };
