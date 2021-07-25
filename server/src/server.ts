42726692e459295fd64cc0ff79a9b96b8cdf9878
vignesh <vignesh-p@outlook.com>
2021-07-25 21:14:48 +0530

    removing unused debug

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,14 +1,12 @@
-import debug from "debug";
-debug("test:server");
-import { config } from "dotenv";
-config({ path: "./.env" });
-import App from "./app";
+import { config } from 'dotenv';
+config({ path: './.env' });
+import App from './app';
 
 const port = normalizePort(process.env.PORT || 3000);
 
 const app = new App();
 
-app.app.set("port", port);
+app.app.set('port', port);
 
 const server = app.server;
 
@@ -16,54 +14,47 @@ server.listen(port);
 server.on('error', onError);
 server.on('listening', onListening);
 
-
-
 //Functions
 function normalizePort(val: any): number | boolean {
-    let port = parseInt(val, 10);
+  let port = parseInt(val, 10);
 
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-    if (port >= 0) {
-        // port number
-        return port;
-    }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
 function onError(error: any): void {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
 
-    let bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
+  let bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
 }
 
 function onListening() {
-    let addr: any = server.address();
-    let bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    debug('Listening on ' + bind);
-    console.log('Listening on ' + bind);
+  let addr: any = server.address();
+  let bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
 }
