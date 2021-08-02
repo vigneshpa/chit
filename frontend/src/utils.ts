84b275d1045ab1ebfa5aed0b054a449a83160d63
vignesh <vignesh-p@outlook.com>
2021-08-02 20:30:59 +0530

    setting default debounce 700 and changing this to any

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,8 +1,11 @@
-export function debounce<Params extends any[], Context extends object> (func:(this:Context, ...args:Params)=>any, delay:number):(this:Context, ...args:Params)=>void{
-  let debounceTimer:any;
-  return function(this:Context, ...args:Params) {
-      const context = this;
-        clearTimeout(debounceTimer);
-        debounceTimer = setTimeout(() => func.apply(context, args), delay)
-  }
+export function debounce<Params extends any[], Context extends any>(
+  func: (this: Context, ...args: Params) => any,
+  delay: number = 700
+): (this: Context, ...args: Params) => void {
+  let debounceTimer: any;
+  return function (this: Context, ...args: Params) {
+    const context = this;
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(() => func.apply(context, args), delay);
+  };
 }
