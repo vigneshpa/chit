c1fbcd0905b5670988d0a83832968817fc12381e
vignesh <vignesh-p@outlook.com>
2021-08-04 17:45:09 +0530

    simplifying global

--- frontend/theme/TTable.svelte
+++ frontend/theme/TTable.svelte
@@ -8,30 +8,32 @@
   @use './scheme.scss' as scheme;
   table {
     width: 100%;
-    :global(th) {
-      text-align: left;
-      border-bottom: 1px solid #ccc;
-    }
-    :global(th),
-    :global(td) {
-      padding: 0.4em;
-      user-select: none;
-    }
-    :global(tr.fold-view:hover) {
-      background-color: scheme.$highlight;
-    }
-    :global(tr.fold.closed div.fold) {
-      max-height: 0px;
-    }
-    :global(tr.fold div.fold) {
-      transition: max-height 0.3s ease;
-      max-height: 400px;
-      padding: 0.4em;
-      overflow: hidden;
-    }
-    :global(td.fold) {
-      padding: 0px;
-      margin: 0px;
+    :global {
+      th {
+        text-align: left;
+        border-bottom: 1px solid #ccc;
+      }
+      th,
+      td {
+        padding: 0.4em;
+        user-select: none;
+      }
+      tr.fold-view:hover {
+        background-color: scheme.$highlight;
+      }
+      tr.fold.closed div.fold {
+        max-height: 0px;
+      }
+      tr.fold div.fold {
+        transition: max-height 0.3s ease;
+        max-height: 400px;
+        padding: 0.4em;
+        overflow: hidden;
+      }
+      td.fold {
+        padding: 0px;
+        margin: 0px;
+      }
     }
   }
 </style>
