0791887cb0778204d7296ff045bbf686c7b99df9
vignesh <vignesh-p@outlook.com>
2021-08-05 13:12:42 +0530

    making justify changable

--- frontend/theme/IconText.svelte
+++ frontend/theme/IconText.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   export let icon: string;
   export let outlined: boolean = false;
+  export let justifyContent: 'left' | 'right' | 'center' = 'center';
 </script>
 
 <template>
-  <div class="t-icon-text-container">
+  <div class="t-icon-text-container" style="justify-content:{justifyContent}">
     <i class="t-icon" class:material-icons={!outlined} class:material-icons-outlined={outlined}> {icon} </i>
     <span class="t-icon-text">
       <slot />
@@ -21,16 +22,15 @@
       height: 24px;
       color: scheme.$textOnBackgroundLight;
       margin-right: 10px;
+      margin-left: 10px;
       overflow: hidden;
     }
     .t-icon-text {
-      margin-top: 0.16rem;
       display: inline-block;
       text-decoration: none;
       padding-right: 10px;
     }
     display: flex;
-    justify-content: center;
     align-items: center;
   }
 </style>
